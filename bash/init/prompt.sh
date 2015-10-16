8662b3b32ebaab40dea1e6aee89b9065fb4f1a79
Johan Ask <jhnesk@gmail.com>
2015-10-16 20:14:58 +0200

    Update PS1

--- bash/init/prompt.sh
+++ bash/init/prompt.sh
@@ -1,6 +1,9 @@
+PS1_BASE="${BWhite}\u@\h${Color_Off} \W"
+
 if [ -r /usr/share/git/git-prompt.sh ]; then
 	. /usr/share/git/git-prompt.sh
-	PS1='[\u@\h \W$(__git_ps1 " ${Yellow}(%s)${Color_Off}")]\$ '
+	GIT_PS1_SHOWDIRTYSTATE=1
+	PROMPT_COMMAND='__git_ps1 "${PS1_BASE}" "\\\$ " " ${Yellow}(%s)${Color_Off}"'
 else
-	PS1='[\u@\h \W]\$ '
+	PS1="${PS1_BASE}\$ "
 fi
