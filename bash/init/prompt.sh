6e1e98083652d9561185a0c4f010e658cb36a85e
Johan Ask <jhnesk@gmail.com>
2015-10-16 19:39:25 +0200

    Check if git prompt is available before using

--- bash/init/prompt.sh
+++ bash/init/prompt.sh
@@ -1,3 +1,6 @@
-. /usr/share/git/git-prompt.sh
-
-PS1='[\u@\h \W$(__git_ps1 " ${Yellow}(%s)${Color_Off}")]\$ '
+if [ -r /usr/share/git/git-prompt.sh ]; then
+	. /usr/share/git/git-prompt.sh
+	PS1='[\u@\h \W$(__git_ps1 " ${Yellow}(%s)${Color_Off}")]\$ '
+else
+	PS1='[\u@\h \W]\$ '
+fi
