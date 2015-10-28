73815620bd3a374193d2f2471c5dac8a48b3c268
Johan Ask <jhnesk@gmail.com>
2015-10-28 22:21:19 +0100

    Trying out new line in bash prompt

--- bash/init/prompt.sh
+++ bash/init/prompt.sh
@@ -3,7 +3,7 @@ PS1_BASE="${BWhite}\u@\h${Color_Off} \W"
 if [ -r /usr/share/git/git-prompt.sh ]; then
 	. /usr/share/git/git-prompt.sh
 	GIT_PS1_SHOWDIRTYSTATE=1
-	PROMPT_COMMAND='__git_ps1 "${PS1_BASE}" "\\\$ " " ${Yellow}(%s)${Color_Off}"'
+	PROMPT_COMMAND='__git_ps1 "${PS1_BASE}" "\\n\\\$ " " ${Yellow}(%s)${Color_Off}"'
 else
-	PS1="${PS1_BASE}\$ "
+	PS1="${PS1_BASE}\n\$ "
 fi
